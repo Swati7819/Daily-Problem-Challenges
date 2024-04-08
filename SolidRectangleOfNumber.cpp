edda285dbdc42154ba787471b3c5ba1588a0faa8
Swati Sharma <swati.sharma.7819@gmail.com>
2024-04-08 21:06:58 +0530

    Add files via upload

---
+++ SolidRectangleOfNumber.cpp
@@ -0,0 +1,20 @@
+// solid rectangle 5*5
+// 10 10 10 10 10 
+// 10 10 10 10 10
+// 10 10 10 10 10
+// 10 10 10 10 10
+// 10 10 10 10 10
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    for(int i=1;i<=5;i++)
+    {
+        for(int j=1;j<=5;j++)
+        {
+            cout<<"10"<<" ";
+        }
+        cout<<endl;
+    }
+}
